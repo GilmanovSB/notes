e271645c9ff9aa0e94598f436b992cd049db0cf1
Sergey <gilmanov10@outlook.com>
2020-10-20 22:43:23 +0300

    new gallary project

--- index.php
+++ index.php
@@ -1,50 +1,49 @@
 <?php
+define('ROOT_DIR', $_SERVER['DOCUMENT_ROOT']);
+define('IMAGE_DIR' , ROOT_DIR . '/pic' . '/');
+define('ROOT', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/');
+$file_name = $_FILES['pic']['name'];
+$file_dir = $_FILES['pic']['tmp_name'];
 
-$field = [
-    [1,1,0,1,1,1,1,1,1,1,1],
-    [1,1,0,1,1,1,1,1,1,1,1],
-    [1,1,0,0,0,0,1,1,1,1,1],
-    [1,1,1,1,1,0,1,1,1,1,1],
-    [1,1,0,0,0,0,1,1,1,1,1],
-    [1,1,0,1,1,1,1,0,0,1,1],
-    [1,1,0,0,1,1,1,0,1,1,1],
-    [1,1,1,0,0,1,1,0,1,1,1],
-    [1,1,1,1,0,0,0,0,1,1,1],
-    [1,1,1,1,1,0,1,1,1,1,1]
-];
 
-function render(array $field) : string
-{
-    $str = "<table>";
-    foreach($field as $valls){
-        $str .= "<tr>";
-        foreach($valls as $vall){
-            $vall == 1 ? $str .= "<td class = 'wall'>" : $str .= "<td>";
-        }
-        $str .= "</tr>";
-    }
-    return $str;
+
+if(isset($_FILES)){
+    move_uploaded_file($file_dir, IMAGE_DIR . $file_name);
 }
 
+function getFiles(string $str): array
+{
+    $fileName = scandir($str);
+    $picName = [];
+    foreach($fileName as $file){
+        if($file == '.' || $file == '..'){
+            false;
+        }   else {
+            $picName[] = $file;
+        }
+    }
+    return $picName;
+}
+
+$pics = getFiles(IMAGE_DIR); 
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-        .wall {
-            background-color: royalblue;
-        }
-        td {
-            width: 10px;
-            height: 10px;
-        }
-    </style>
     <title>Document</title>
 </head>
 <body>
-    <?php echo render($field);?>
+    <form action="" method="post" enctype="multipart/form-data">
+        <input type="file" name = 'pic'>
+        <input type="submit">
+    </form>
+    
+    <?php foreach($pics as $pic):?>
+    <img src="<?= ROOT . "pic/" . $pic?>" alt="$" width="250">
+    <?php endforeach;?>
 </body>
 </html>
